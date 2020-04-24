3c5e41669d7b9690d55dadd0d31af287b9b9c578
ChoiChanHee <63842650+ChoiChanHee@users.noreply.github.com>
2020-04-25 03:38:33 +0900

    Update extras.h

--- extras.h
+++ extras.h
@@ -7,4 +7,9 @@
 int defragment(Record a[], int n, int *c);
 void sorting_data(Record a[], int c);
 void the_most_like_genre(Record a[], int c);
+int Login(Record a[], int c);
+void add_book(Record a[], int n, int c);
+void show_lib(Record a[], int n, int c);
+void delete_book(Record a[], int n, int c);
+void show_lib(Record a[], int n, int c);
 #endif
